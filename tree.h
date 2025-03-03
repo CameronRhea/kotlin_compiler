00ca269121816a1fcd489a1eb31faca607bb3565
CameronRhea <cameron.rhea@student.nmt.edu>
2025-03-02 23:15:25 -0700

    lab5 tree files changes

--- tree.h
+++ tree.h
@@ -1,7 +1,28 @@
-struct tree {
-   int prodrule;
-   char *symbolname;
-   int nkids;
-   struct tree *kids[10]; /* if nkids >0 */
-   struct token *leaf;   /* if nkids == 0; NULL for ε productions */
+#ifndef TREE_H
+#define TREE_H
+
+struct token {
+    int category;
+    char *text;
+    int lineno;
+    char *filename;
+    int ival;
+    double dval;
+    char *sval;
 };
+
+struct tree {
+    int prodrule;
+    char *symbolname;
+    int nkids;
+    int id;
+    struct tree *kids[9];
+    struct token *leaf;
+};
+
+extern struct tree *root;
+
+struct tree *create_node(int prodrule, char *symbolname, int nkids);
+void free_tree(struct tree *t);
+
+#endif
